44bf3062db03c0afa0cef39800b81823e3df9db1
Rubens Pessoa <pessoadebarros@gmail.com>
2014-07-18 22:55:59 -0300

    Atualizando main.py para que a conexão não seja interrompida. Deletando db.sqlite .

--- LearningDjango/main.py
+++ LearningDjango/main.py
@@ -4,4 +4,7 @@ from movie_info.models import Movie, Genre, Country, Language, Director
 from crawler import fill_db_movie_info
 
 if __name__ == '__main__':
-    fill_db_movie_info('/Users/rubenspessoa/Documents/Workspace/DATASET/test_subdataset.csv')
+	while True:
+    	fill_db_movie_info('/Users/rubenspessoa/Documents/Workspace/DATASET/test_subdataset.csv')
+    except:
+    	continue
