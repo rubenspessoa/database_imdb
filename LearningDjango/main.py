19102b1f4fac78a85ab9291deaf947a8b28a4b36
Rubens Pessoa <pessoadebarros@gmail.com>
2014-07-18 23:00:49 -0300

    Add DATASETS folder

--- LearningDjango/main.py
+++ LearningDjango/main.py
@@ -3,8 +3,11 @@
 from movie_info.models import Movie, Genre, Country, Language, Director
 from crawler import fill_db_movie_info
 
+### HERE YOU CHANGE FOR THE PATH WHERE test_subdataset.csv IS LOCATED ON YOUR PC ###
+PATH = '/Users/rubenspessoa/Documents/Workspace/DATASET/'
+
 if __name__ == '__main__':
 	while True:
-    	fill_db_movie_info('/Users/rubenspessoa/Documents/Workspace/DATASET/test_subdataset.csv')
+    	fill_db_movie_info(PATH + 'test_subdataset.csv')
     except:
     	continue
